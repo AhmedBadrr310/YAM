8c52dc3a370c346eaf521f0cb3375eb09e4553b8
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2025-04-21 07:36:25 +0200

    Add project files.

---
+++ Yam.AppHost/Program.cs
@@ -0,0 +1,7 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddProject<Projects.Yam_AuthService>("yam-authservice");
+
+builder.AddProject<Projects.Yam_NotificationService>("yam-notificationservice");
+
+builder.Build().Run();
---
+++ Yam.Core/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yam.Core.Entities
+{
+    public class User
+    {
+        public string? Username { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? PasswordHashed { get; set; }
+
+        public IReadOnlyList<byte> ProfilePicture { get; set; } = new List<byte>();
+
+        public string? Gender { get; set; }
+
+        public DateOnly BirthDate{ get; set; }
+
+        public DateOnly JoinDate { get; set; }
+
+        public string? Bio { get; set; }
+    }
+}
